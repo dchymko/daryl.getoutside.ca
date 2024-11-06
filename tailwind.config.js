c8fb688a751a55497646220c8d77a3520ce4c780
Daryl Chymko <daryl@getoutside.ca>
2024-11-06 05:56:35 -0800

    Update Tailwind refs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
+//--------------------------------------------------------------------------
+// Tailwind configuration
+//--------------------------------------------------------------------------
+//
+// Use the Tailwind configuration to completely define the current sites
+// design system by adding and extending to Tailwinds default utility
+// classes. Various aspects of the config are split inmultiple files.
+//
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  // The various configurable Tailwind configuration files.
+  presets: [
+    require('tailwindcss/defaultConfig'),
+    require('./tailwind.config.typography.js'),
+    require('./tailwind.config.peak.js'),
+    require('./tailwind.config.site.js'),
+  ],
+  // Configure files to scan for utility classes (JIT).
   content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
+    './resources/views/**/*.blade.php',
+    './resources/views/**/*.html',
+    './resources/js/**/*.js',
+    './content/**/*.md',
+    './vendor/studio1902/**/*.blade.php',
+    './vendor/studio1902/**/*.html',
+    './vendor/studio1902/**/*.js',
   ],
   theme: {
     extend: {
@@ -15,4 +36,5 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
   ],
+  safelist: []
 }
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/tailwind.css',
-                'resources/js/site.js',
-            ],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: (id) => {
+                        if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
+        plugins: [
+            laravel({
+                refresh: true,
+                input: [
+                    'resources/css/site.css',
+                    'resources/css/tailwind.css',
+                    'resources/css/peak.site.css',
+                    'resources/js/site.js',
+                ]
+            })
+        ],
+        server: {
+            open: env.APP_URL
+        }
+    }
 });
